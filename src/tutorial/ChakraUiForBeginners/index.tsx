9a400265a24646bdee2b2a5001fc6e92b564968a
shun <shunmel.09@gmail.com>
2023-03-30 09:04:07 +0900

    tutorial videoを使った学習 Lesson12

---
+++ src/tutorial/ChakraUiForBeginners/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import Header from './Header';
+
+const ChakraUiForBeginners = () => {
+  return (
+    <Box textAlign="center" margin="0 auto">
+      <Header />
+    </Box>
+  );
+};
+
+export default ChakraUiForBeginners;
